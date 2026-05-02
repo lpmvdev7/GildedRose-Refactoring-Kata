08c5f06e9600643d2c9f933046b846b52677852c
lpmvdev7 <lpmvdev7@gmail.com>
2026-05-02 12:45:39 -0600

    Micro-paso I1 — Extraer clase ItemUpdater

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,76 +1,108 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
+class ItemUpdater:
+    def __init__(self, item):
+        self.item = item
 
-    AGED_BRIE = "Aged Brie"
-    SULFURAS = "Sulfuras, Hand of Ragnaros"
-    BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+    def update(self):
+        if self.is_sulfuras():
+            return
+
+        self.update_quality()
+        self.decrease_sell_in()
+
+        if self.is_expired():
+            self.update_expired()
+
+    # --- Template methods (hooks) ---
+
+    def update_quality(self):
+        self.decrease_quality()
+
+    def update_expired(self):
+        self.decrease_quality()
+
+    # --- Helpers comunes ---
+
+    def decrease_sell_in(self):
+        self.item.sell_in -= 1
+
+    def is_expired(self):
+        return self.item.sell_in < 0
+
+    def is_sulfuras(self):
+        return self.item.name == "Sulfuras, Hand of Ragnaros"
+
+    def increase_quality(self):
+        if self.item.quality < 50:
+            self.item.quality += 1
+
+    def decrease_quality(self):
+        if self.item.quality > 0:
+            self.item.quality -= 1
+
+
+# --- Implementaciones específicas ---
+
+class AgedBrieUpdater(ItemUpdater):
+
+    def update_quality(self):
+        self.increase_quality()
+
+    def update_expired(self):
+        self.increase_quality()
+
+
+class BackstagePassUpdater(ItemUpdater):
+
+    def update_quality(self):
+        self.increase_quality()
+
+        if self.item.sell_in < 11:
+            self.increase_quality()
+
+        if self.item.sell_in < 6:
+            self.increase_quality()
+
+    def update_expired(self):
+        self.item.quality = 0
+
+
+class NormalItemUpdater(ItemUpdater):
+    pass
+
+
+class SulfurasUpdater(ItemUpdater):
+    def update(self):
+        # No cambia nada
+        return
+
+
+# --- Factory simple ---
+
+class ItemUpdaterFactory:
+    @staticmethod
+    def get_updater(item):
+        if item.name == "Aged Brie":
+            return AgedBrieUpdater(item)
+        if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            return BackstagePassUpdater(item)
+        if item.name == "Sulfuras, Hand of Ragnaros":
+            return SulfurasUpdater(item)
+        return NormalItemUpdater(item)
+
+
+# --- Clase principal ---
+
+class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-
-            if self._is_sulfuras(item):
-                continue
-
-            if item.name == self.AGED_BRIE:
-                self._update_aged_brie(item)
-            elif item.name == self.BACKSTAGE_PASSES:
-                self._update_backstage_pass(item)
-            else:
-                self._update_normal_item(item)
-
-            item.sell_in -= 1
-
-            if item.sell_in < 0:
-                self._handle_expired(item)
-
-    # --- Métodos por tipo ---
-
-    def _update_aged_brie(self, item):
-        self._increase_quality(item)
-
-    def _update_backstage_pass(self, item):
-        self._increase_quality(item)
-
-        if item.sell_in < 11:
-            self._increase_quality(item)
-
-        if item.sell_in < 6:
-            self._increase_quality(item)
-
-    def _update_normal_item(self, item):
-        self._decrease_quality(item)
-
-    # --- Guard clauses aplicadas aquí ---
-
-    def _handle_expired(self, item):
-        if item.name == self.BACKSTAGE_PASSES:
-            item.quality = 0
-            return
-
-        if item.name == self.AGED_BRIE:
-            self._increase_quality(item)
-            return
-
-        self._decrease_quality(item)
-
-    def _is_sulfuras(self, item):
-        return item.name == self.SULFURAS
-
-    # --- Helpers ---
-
-    def _increase_quality(self, item):
-        if item.quality >= 50:
-            return
-        item.quality += 1
-
-    def _decrease_quality(self, item):
-        if item.quality <= 0:
-            return
-        item.quality -= 1
+            updater = ItemUpdaterFactory.get_updater(item)
+            updater.update()
 
 
 class Item:
